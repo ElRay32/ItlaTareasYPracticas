3d22797d1e47f65604093f5cee9127be3a75e941
ElRay32 <guerrero20191@outlook.es>
2025-02-02 22:36:09 -0400

    Tarea 2, Cree un programa sencillo.
    Programa sencillo al colocar un numero te dice si es par o impar

---
+++ Tarea2/Tarea2CrearPrimerPrograma/Tarea2CrearPrimerPrograma/Program.cs
@@ -0,0 +1,14 @@
+﻿// Raymel Enrique Guerrero Reynoso 2023-1677
+
+Console.WriteLine("Digite el numero: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Numero es Par.");
+}
+else
+{
+    Console.WriteLine("Numero es Impar.");
+}
