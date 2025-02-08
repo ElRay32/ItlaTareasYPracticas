e862174a73653d4bb4c01b4029d5c6836dc3a59f
ElRay32 <guerrero20191@outlook.es>
2025-02-07 20:45:33 -0400

    Tarea 3, Desarrolle Metodos y/o funciones para la Calculadora

---
+++ Tarea4-Calculadora Sencilla/Calculadora Sencilla/Calculadora Sencilla/Program.cs
@@ -0,0 +1,68 @@
+﻿
+decimal number1;
+decimal number2;
+string opcionOperation;
+
+try { 
+Console.WriteLine("Calculadora Sencilla");
+Console.WriteLine("Digite el numero de que operacion quiere realizar: 1.Suma, 2.Resta, 3.Multiplicacion, 4.Division. ");
+opcionOperation = Console.ReadLine();
+
+
+Console.WriteLine("Inserte el primer numero que quieres calcular(Dato numerico tiene que ser digitado)");
+number1 = Convert.ToDecimal(Console.ReadLine());
+
+
+Console.WriteLine("Inserte el segundo numero que quieres calcular(Dato numerico tiene que ser digitado)");
+number2 = Convert.ToDecimal(Console.ReadLine());
+
+decimal result = 0;
+
+switch (opcionOperation)
+{
+    case "1":
+        {
+            result = number1 + number2;
+            break;
+        }
+    case "2":
+        {
+            result = number2 - number1;
+            break;
+        }
+    case "3":
+        {
+            result = number1 * number2;
+            break;
+        }
+    case "4":
+        {
+            result = number1 / number2;
+            break;
+        }
+    default:
+        result = 0;
+        break;
+}
+
+Console.WriteLine($"Resultado:  {result}");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (ArithmeticException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (FormatException ex)
+{
+    Console.WriteLine($"No tiene el formato correcto");
+}
+catch (Exception ex) {
+    Console.WriteLine(ex.Message); 
+}
+
+
+
+Console.ReadKey();
